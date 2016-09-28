2984099cb849ef6e7db763fa18aa2e8a137b8f16
Alessandro Aleotti <71196@studenti.unimore.it>
2016-09-28 16:03:34 +0200

    database corretto e funzionante su Postgres 9.5

--- DBmodel/socialDB.sql
+++ DBmodel/socialDB.sql
@@ -7,107 +7,140 @@
 
 -- Database creation must be done outside an multicommand file.
 -- These commands were put in this file only for convenience.
-object: "socialDB" | type: DATABASE
-DROP DATABASE IF EXISTS "socialDB";
-CREATE DATABASE "socialDB"
-;
+-- -- object: "distributedSocialDB" | type: DATABASE --
+-- -- DROP DATABASE IF EXISTS "distributedSocialDB";
+-- CREATE DATABASE "distributedSocialDB"
+-- 	OWNER = postgres
+-- ;
 -- -- ddl-end --
 -- 
 
--- object: public."myPost" | type: TABLE --
--- DROP TABLE IF EXISTS public."myPost" CASCADE;
-CREATE TABLE "myPost"(
-	"postID" timestamp NOT NULL,
-	"userID" uuid,
-	CONSTRAINT "PK" PRIMARY KEY ("postID")
+-- object: public.known_nodes | type: TABLE --
+DROP TABLE IF EXISTS public.known_nodes CASCADE;
+CREATE TABLE public.known_nodes (
+  user_id     UUID NOT NULL,
+  username    VARCHAR(16),
+  address     CIDR,
+  port        SMALLINT,
+  last_update TIMESTAMP,
+  CONSTRAINT "PK" PRIMARY KEY (user_id)
 
 );
 -- ddl-end --
-ALTER TABLE public."myPost" OWNER TO postgres;
+ALTER TABLE public.known_nodes
+  OWNER TO postgres;
 -- ddl-end --
 
--- object: public."knownNodes" | type: TABLE --
--- DROP TABLE IF EXISTS public."knownNodes" CASCADE;
-CREATE TABLE public."knownNodes"(
-	"userID" uuid NOT NULL,
-	"nodeIP" cidr,
-	"portNumber" int4,
-	username text,
-	CONSTRAINT "PK_2" PRIMARY KEY ("userID")
+-- object: public.friends | type: TABLE --
+DROP TABLE IF EXISTS public.friends CASCADE;
+CREATE TABLE public.friends (
+  user_id_known_nodes UUID,
+  CONSTRAINT friends_pk PRIMARY KEY (user_id_known_nodes)
 
 );
 -- ddl-end --
-ALTER TABLE public."knownNodes" OWNER TO postgres;
+ALTER TABLE public.friends
+  OWNER TO postgres;
 -- ddl-end --
 
--- object: public.comment | type: TABLE --
--- DROP TABLE IF EXISTS public.comment CASCADE;
-CREATE TABLE public.comment(
-	"userID" uuid,
-	"timestamp" timestamp,
-	content text,
-	"postID" integer,
+-- object: known_nodes_fk | type: CONSTRAINT --
+ALTER TABLE public.friends
+  DROP CONSTRAINT IF EXISTS known_nodes_fk CASCADE;
+ALTER TABLE public.friends
+  ADD CONSTRAINT known_nodes_fk FOREIGN KEY (user_id_known_nodes)
+REFERENCES public.known_nodes (user_id) MATCH FULL
+ON DELETE CASCADE ON UPDATE CASCADE;
+-- ddl-end --
+
+-- object: public.comments | type: TABLE --
+DROP TABLE IF EXISTS public.comments CASCADE;
+CREATE TABLE public.comments (
+  comment_id TIMESTAMP NOT NULL,
+  content    VARCHAR(512),
+  user_id    UUID,
+  post_id    TIMESTAMP,
+  CONSTRAINT comments_pk PRIMARY KEY (comment_id)
+);
+-- ddl-end --
+ALTER TABLE public.comments
+  OWNER TO postgres;
+-- ddl-end --
+
+-- object: public.posts | type: TABLE --
+DROP TABLE IF EXISTS public.posts CASCADE;
+CREATE TABLE public.posts (
+  post_id TIMESTAMP NOT NULL,
+  user_id UUID,
+  CONSTRAINT post_pk PRIMARY KEY (post_id)
+);
+-- ddl-end --
+ALTER TABLE public.posts
+  OWNER TO postgres;
+-- ddl-end --
+
+-- object: public.post_img | type: TABLE --
+DROP TABLE IF EXISTS public.post_img CASCADE;
+CREATE TABLE public.post_img (
+  path_to_file VARCHAR(512)
+  --   post_id      TIMESTAMP NOT NULL,
+  --   user_id      UUID
+)
+  INHERITS (public.posts);
+-- ddl-end --
+ALTER TABLE public.post_img
+  OWNER TO postgres;
+-- ddl-end --
+
+-- object: public.post_text | type: TABLE --
+DROP TABLE IF EXISTS public.post_text CASCADE;
+CREATE TABLE public.post_text (
+  content VARCHAR(512)
+  --   post_id timestamp NOT NULL,
+  --   user_id uuid
+)
+  INHERITS (public.posts);
+-- ddl-end --
+ALTER TABLE public.post_text
+  OWNER TO postgres;
+-- ddl-end --
+
+-- object: public.my_user | type: TABLE --
+DROP TABLE IF EXISTS public.my_user CASCADE;
+CREATE TABLE public.my_user (
+  user_id  UUID NOT NULL,
+  username VARCHAR(16),
+  CONSTRAINT my_user_pk PRIMARY KEY (user_id)
 
 );
 -- ddl-end --
-ALTER TABLE public.comment OWNER TO postgres;
+ALTER TABLE public.my_user
+  OWNER TO postgres;
 -- ddl-end --
 
--- object: public.friend | type: TABLE --
--- DROP TABLE IF EXISTS public.friend CASCADE;
-CREATE TABLE public.friend(
-	"userID_knownNodes" uuid NOT NULL,
-	CONSTRAINT friend_pk PRIMARY KEY ("userID_knownNodes")
-
-);
--- ddl-end --
-ALTER TABLE public.friend OWNER TO postgres;
--- ddl-end --
-
--- object: public."myUser" | type: TABLE --
--- DROP TABLE IF EXISTS public."myUser" CASCADE;
-CREATE TABLE public."myUser"(
-	"myID" uuid NOT NULL,
-	"myUsername" text,
-	"myAddress" cidr,
-	"myPort" smallint,
-	CONSTRAINT pk PRIMARY KEY ("myID")
-
-);
--- ddl-end --
-ALTER TABLE public."myUser" OWNER TO postgres;
--- ddl-end --
-
--- object: "knownNodes_fk" | type: CONSTRAINT --
--- ALTER TABLE public.friend DROP CONSTRAINT IF EXISTS "knownNodes_fk" CASCADE;
-ALTER TABLE public.friend ADD CONSTRAINT "knownNodes_fk" FOREIGN KEY ("userID_knownNodes")
-REFERENCES public."knownNodes" ("userID") MATCH FULL
-ON DELETE RESTRICT ON UPDATE CASCADE;
--- ddl-end --
-
--- object: friend_uq | type: CONSTRAINT --
--- ALTER TABLE public.friend DROP CONSTRAINT IF EXISTS friend_uq CASCADE;
-ALTER TABLE public.friend ADD CONSTRAINT friend_uq UNIQUE ("userID_knownNodes");
--- ddl-end --
-
--- object: "fkUserID" | type: CONSTRAINT --
--- ALTER TABLE public."myPost" DROP CONSTRAINT IF EXISTS "fkUserID" CASCADE;
-ALTER TABLE public."myPost" ADD CONSTRAINT "fkUserID" FOREIGN KEY ("userID")
-REFERENCES public."myUser" ("myID") MATCH FULL
+-- object: comments_post_id_fk | type: CONSTRAINT --
+ALTER TABLE public.comments
+  DROP CONSTRAINT IF EXISTS comments_post_id_fk CASCADE;
+ALTER TABLE public.comments
+  ADD CONSTRAINT comments_post_id_fk FOREIGN KEY (post_id)
+REFERENCES public.posts (post_id) MATCH FULL
 ON DELETE NO ACTION ON UPDATE NO ACTION;
 -- ddl-end --
 
--- object: "fkPostID" | type: CONSTRAINT --
--- ALTER TABLE public.comment DROP CONSTRAINT IF EXISTS "fkPostID" CASCADE;
-ALTER TABLE public.comment ADD CONSTRAINT "fkPostID" FOREIGN KEY ("postID")
-REFERENCES public."myPost" ("postID") MATCH FULL
+-- object: comments_user_id_fk | type: CONSTRAINT --
+ALTER TABLE public.comments
+  DROP CONSTRAINT IF EXISTS comments_user_id_fk CASCADE;
+ALTER TABLE public.comments
+  ADD CONSTRAINT comments_user_id_fk FOREIGN KEY (user_id)
+REFERENCES public.friends (user_id_known_nodes) MATCH FULL
 ON DELETE NO ACTION ON UPDATE NO ACTION;
 -- ddl-end --
 
--- object: "fkUserID" | type: CONSTRAINT --
--- ALTER TABLE public.comment DROP CONSTRAINT IF EXISTS "fkUserID" CASCADE;
-ALTER TABLE public.comment ADD CONSTRAINT "fkUserID" FOREIGN KEY ("userID")
-REFERENCES public.friend ("userID_knownNodes") MATCH FULL
+-- object: post_user_id_fk | type: CONSTRAINT --
+ALTER TABLE public.posts
+  DROP CONSTRAINT IF EXISTS post_user_id_fk CASCADE;
+ALTER TABLE public.posts
+  ADD CONSTRAINT post_user_id_fk FOREIGN KEY (user_id)
+REFERENCES public.my_user (user_id) MATCH FULL
 ON DELETE NO ACTION ON UPDATE NO ACTION;
 -- ddl-end --
 
