951e314243da89bfb6c28337ea30fbe37c6ce593
Francesco-Prandini <74424@studenti.unimore.it>
2016-10-11 11:55:07 +0200

    Ho aggiunto una nuova versione del metodo open_connection

--- DbManagement/db_openConnection.py
+++ DbManagement/db_openConnection.py
@@ -10,3 +10,16 @@ def open_connecton():
         host='localhost')
 
     return dbpool
+
+
+from DbManagement.db_insertor import *
+
+
+def openConnectionOnDB(database,port,username,password,host):
+    args=[database,port,username,password,host]
+    arg_string="dbname="+str(database)+" port="+str(port)+" password="+str(password)+" host="+str(host)
+    print(args)
+    dbpool = adbapi.ConnectionPool("psycopg2",arg_string)
+
+    d=DatabaseInsertor(dbpool)
+    d.insert_my_user(username)
