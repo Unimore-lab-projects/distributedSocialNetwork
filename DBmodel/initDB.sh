c681cbb4c58daba11a4d2ec6d75d7251ba517155
Francesco-Prandini <74424@studenti.unimore.it>
2016-10-16 17:48:54 +0200

    Script per automatizzare la creazione del db

---
+++ DBmodel/initDB.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+dropdb db_peer1
+dropdb db_peer2
+dropdb db_peer3
+dropdb db_peer4
+
+
+createdb -O peer1 db_peer1
+createdb -O peer2 db_peer2
+createdb -O peer3 db_peer3
+createdb -O peer4 db_peer4
+
+
+psql -d db_peer1 -U peer1 -h 127.0.0.1 -f $1
+psql -d db_peer2 -U peer2 -h 127.0.0.1 -f $1
+psql -d db_peer3 -U peer3 -h 127.0.0.1 -f $1
+psql -d db_peer4 -U peer4 -h 127.0.0.1 -f $1
