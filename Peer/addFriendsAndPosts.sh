232f8186496d47d5e7eadeb1597b1ba7d4eb6095
Francesco-Prandini <74424@studenti.unimore.it>
2017-05-30 16:04:18 +0200

    + mainInsertPost.py

--- Peer/addFriendsAndPosts.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-
-psql -d db_peer1 -U peer1 -h 127.0.0.1 -c "insert into Friends values('cb21a82e-4828-4c80-83cc-a5bdf3efad4e','peer3',1)"
-psql -d db_peer1 -U peer1 -h 127.0.0.1 -c "insert into Friends values('6f7cd9bd-31fb-4e7e-9efd-6dc53b0b38b5','peer4',3)"
-psql -d db_peer3 -U peer3 -h 127.0.0.1 -c "insert into Posts values('cb21a82e-4828-4c80-83cc-a5bdf3efad4e','','post1',20,to_timestamp(NOW()))"
-psql -d db_peer4 -U peer4 -h 127.0.0.1 -c "insert into Posts values('6f7cd9bd-31fb-4e7e-9efd-6dc53b0b38b5','','post2',21,to_timestamp(NOW()))"
-psql -d db_peer4 -U peer4 -h 127.0.0.1 -c "insert into Posts values('6f7cd9bd-31fb-4e7e-9efd-6dc53b0b38b5','','post3',22,to_timestamp(NOW()))"
--- Peer/addNodesToResolve.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-
-psql -d db_peer4 -U peer4 -h 127.0.0.1 -c "insert into Known_nodes values('83c2e8bb-317d-4a68-b441-e14756439a74','127.0.0.1',8005,NOW(),3)"
-psql -d db_peer4 -U peer4 -h 127.0.0.1 -c "insert into Known_nodes values('4d3da9b3-ec1f-4628-90e5-dba04603331f','127.0.0.1',8006,NOW(),1)"
-psql -d db_peer4 -U peer4 -h 127.0.0.1 -c "insert into Known_nodes values('54b8dffd-8011-4c1a-9614-77b25c1e44c7','127.0.0.1',8007,NOW(),4)"
