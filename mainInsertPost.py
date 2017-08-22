875d80071ea916793578ce1ebec5d806a71b1e69
Alessandro Aleotti <71196@studenti.unimore.it>
2017-08-22 21:42:14 +0200

    modificato insertpost

--- mainInsertPost.py
+++ mainInsertPost.py
@@ -1,16 +1,21 @@
-
-
 from backend.Peer.node import *
 import sys
 
+
 def main():
     print("launched mainInsertPost")
-    this=node(sys.argv[1])
+    this = node(sys.argv[1])
     print("launch insert posts")
-    this.insertor.insert_post(None,"Post1")
-    this.insertor.insert_post(None,"Post2")
+    this.insertor.insert_post(None, "Post1")
+    this.insertor.insert_post(None, "Post2")
     print("end insert posts")
+
+    ##reactor. si chiude entro 10 secondi e termina
+    reactor.callLater(4, reactor.stop)
+
     reactor.run()
 
-if __name__=='__main__':
+
+
+if __name__ == '__main__':
     main()
